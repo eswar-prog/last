41f5005622bd9bc4f56708328f48ccfc38a0db1b
Pradhishwar Raj .k <94003516+eswar-prog@users.noreply.github.com>
2025-01-10 14:33:28 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://eswar.up.railway.app/api" : "/api",
+  baseURL: "https://eswar.up.railway.app/api",
   withCredentials: true,
 });
