cd668ad1d9fa86eca6aecb6623cb33bd996fc745
Pradhishwar Raj .k <94003516+eswar-prog@users.noreply.github.com>
2025-01-18 11:40:58 +0530

    Update useAuthStore.js

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "https://eswar.up.railway.app/";
+const BASE_URL = "https://back-test-3nrb.onrender.com/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
